c06537e3a859aacb7a25efb2460077ec0f79414d
zichen1 <zichen1@student.unimelb.edu.au>
2025-01-08 13:14:32 +1100

    Set up project

---
+++ bike_simulation_bgl_jason_li/src/bike_simulation_bgl_jason_li/Main.java
@@ -0,0 +1,10 @@
+package bike_simulation_bgl_jason_li;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bike Simulation Hello World");
+	}
+
+}
---
+++ bike_simulation_bgl_jason_li/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module bike_simulation_bgl_jason_li {
+}
